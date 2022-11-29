0484080b97bad8291f5a9bda4d7929f06f1bd679
L3 <lana.leishman@gmail.com>
2022-11-28 18:59:58 -0700

    commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+   e.preventDefault();
+   document.querySelector('#my-form').getElementsByClassName.background =
+   '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+});
