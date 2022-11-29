a7acd94caa1af1c782dbcdb1b818a8a423424b71
L3 <lana.leishman@gmail.com>
2022-11-28 19:16:28 -0700

    commit

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const btn = document.querySelector('.btn');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-btn.addEventListener('click', (e) => {
-   e.preventDefault();
-   document.querySelector('#my-form').style.background =
-   '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items')
-    .lastElementChild.innerHTML = '<h1>Hello</h1>';
-});
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    console.log(nameInput);
+    }
